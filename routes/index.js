f5673919e98dcae0681484a2a88fe3f1f7ec0048
mj51095 <mj51095@fer.hr>
2021-11-07 23:24:46 +0100

    Dodana mapa

--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,9 @@
 var router = require('express').Router();
 const { requiresAuth } = require('express-openid-connect');
 
+var locList = [
+]
+
 router.get('/', function (req, res, next) {
   res.render('index', {
     title: 'Auth0 Webapp sample Nodejs',
@@ -13,6 +16,14 @@ router.get('/profile', requiresAuth(), function (req, res, next) {
     userProfile: JSON.stringify(req.oidc.user, null, 2),
     title: 'Profile page'
   });
+});
+
+router.post('/location', requiresAuth(), function (req, res, next) {
+  console.log(req.body);
+  if (Object.keys(req.body).length !== 0 && locList.push(req.body) > 5) {
+    locList.shift();
+  }
+  return res.status(200).json(locList);
 });
 
 module.exports = router;
